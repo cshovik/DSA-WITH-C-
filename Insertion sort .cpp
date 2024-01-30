a872764e96fc21f5332364233d26be55dda2c6fe
SHOVIK CHAKRABORTY <113230439+cshovik@users.noreply.github.com>
2024-01-30 10:40:04 +0530

    Create Insertion sort .cpp

---
+++ Insertion sort .cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[1000];
+    int n;
+    cout<<"Enter the size of Array:";
+    cin>>n;
+    for (int i=0; i<n;i++){
+        cin>> arr[i];
+    }
+
+    for(int i=1; i<n; i++){
+        for(int j=i; j>0; j--){
+            if(arr[j]< arr[j-1]){
+                 int temp =arr[j];
+                 arr[j] = arr[j-1];
+                 arr[j-1] =temp;
+            }else{
+                break;
+            }
+
+        }
+    }
+
+    for(int i=0;i<n; i++){
+        cout<< arr[i]<<" ";
+    }
+    
+}
