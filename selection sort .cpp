bf3aca133613273f08f559da365a0be57d69a9d9
SHOVIK CHAKRABORTY <113230439+cshovik@users.noreply.github.com>
2024-06-27 13:50:14 +0530

    Create selection sort .cpp

---
+++ selection sort .cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[1000];
+    int n;
+    cin>>n;
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    for(int i=0; i<n-2; i++){
+        int index =i;
+        for(int j=i+1; j<n; j++){
+            if(arr[j]< arr[index]){
+                index=j;
+            }
+        }
+        swap(arr[i],arr[index]);
+    }
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    
+}
